af4cbd07ca9f685ed7d02952f237d5ac1e90ac3e
wzz <ywc0008@naver.com>
2024-07-05 09:10:47 +0900

    baseUrl 변경

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
   url: "https://ywc0008.github.io/wzzBlog/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/wzzBlog/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
