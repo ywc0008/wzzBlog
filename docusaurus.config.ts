1ef951d443d48b2e6b466c04542dab13651b92e9
wzz <ywc0008@naver.com>
2024-07-05 23:30:19 +0900

    글귀 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -52,10 +52,10 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
-        gtag: {
-          trackingID: "G-RRC6ZVV615",
-          anonymizeIP: true,
-        },
+        // gtag: {
+        //   trackingID: "G-RRC6ZVV615",
+        //   anonymizeIP: true,
+        // },
         googleTagManager: {
           containerId: "GTM-583X5B8R",
         },
